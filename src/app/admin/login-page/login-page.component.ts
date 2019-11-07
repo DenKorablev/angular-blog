572365c2b148b57f07e84200a84f22c7a1977b87
DenKorablev <denis18051990@mail.ru>
2019-11-07 12:59:33 +0300

    Lesson - 143

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { User } from '../shared/interfaces';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -10,10 +12,21 @@ import { User } from '../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submited: boolean = false;
+  message: string;
 
-  constructor() {}
+  constructor(
+    public authServie: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = "Пожалуйста введите данные!"
+      }
+    });
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -21,7 +34,7 @@ export class LoginPageComponent implements OnInit {
       ]),
       password: new FormControl(null, [
         Validators.required,
-        Validators.minLength(6)
+        Validators.minLength(3)
       ])
     })
   }
@@ -30,9 +43,19 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+
+    this.submited = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
-    }
+    };
+    this.authServie.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+      this.submited = false;
+    }, () => {
+      this.submited = false;
+    });
   }
 }
